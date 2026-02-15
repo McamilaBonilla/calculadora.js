a0f77980b362e73d3ee398ce66c7e647b917b814
McamilaBonilla <Mariac.bonillam@uqvirtual.edu.co>
2026-02-14 23:00:21 -0500

    Primer commit calculadora

---
+++ calculadora.js
@@ -0,0 +1,32 @@
+console.log("Hola, esta es una nueva calculadora básica");
+
+let opcion = prompt("Elige una opción:\n1 para suma\n2 para resta\n3 para multiplicación\n4 para división");
+
+let numero1 = parseFloat(prompt("Ingresa el primer número"));
+let numero2 = parseFloat(prompt("Ingresa el segundo número"));
+
+let resultado;
+
+if (opcion == "1") {
+    resultado = numero1 + numero2;
+    alert("El resultado de la suma es: " + resultado);
+}
+else if (opcion == "2") {
+    resultado = numero1 - numero2;
+    alert("El resultado de la resta es: " + resultado);
+}
+else if (opcion == "3") {
+    resultado = numero1 * numero2;
+    alert("El resultado de la multiplicación es: " + resultado);
+}
+else if (opcion == "4") {
+    if (numero2 != 0) {
+        resultado = numero1 / numero2;
+        alert("El resultado de la división es: " + resultado);
+    } else {
+        alert("No se puede dividir entre cero");
+    }
+}
+else {
+    alert("Opción no válida");
+}
